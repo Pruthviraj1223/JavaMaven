96e1134ee9178cb2f639ebf3ba89747ffec86442
Pruthviraj1223 <67500814+Pruthviraj1223@users.noreply.github.com>
2022-03-29 23:52:08 +0530

    Add files via upload

--- Serversocket.java
+++ Serversocket.java
@@ -1,41 +1,44 @@
-package com.company.oop;
-
-import netscape.javascript.JSObject;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Collection;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Serversocket {
 
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serversocket= new ServerSocket(6666);
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ServerSocket serversocket = new ServerSocket(6666);
         System.out.println("accepting...");
         Socket s = serversocket.accept();
         System.out.println("accepted");
 
-        InputStreamReader inputStreamReader = new InputStreamReader(s.getInputStream());
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(s.getOutputStream());
 
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+        InputStream inputStream = new DataInputStream(s.getInputStream());
 
+        ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(inputStream.readAllBytes()));
 
+        Map<Integer, String> data2 = (Map<Integer, String>) objectInputStream.readObject();
+        System.out.println("data = " + data2.toString());
 
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("pruthvi","JAdeja");
+        hashMap.put("jsj","jdsj");
 
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            bufferedWriter.write(scanner.nextLine());
-            bufferedWriter.newLine();
-            bufferedWriter.flush();
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(byteOut);
+        out.writeObject(hashMap);
+        out.flush();
 
-            String output = bufferedReader.readLine();
+        DataOutputStream dataOutputStream = new DataOutputStream(s.getOutputStream());
+        dataOutputStream.write(byteOut.toByteArray());
+        dataOutputStream.flush();
 
-            System.out.println("client = " );
-        }
 
 
     }
